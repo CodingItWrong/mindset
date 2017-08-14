7ed55f123849a1e5aa8036db796623d5f90ad38a
Josh Justice <jjustice@bignerdranch.com>
2017-08-14 06:06:32 -0400

    Split up feature tour spec into step methods

--- spec/features/viewing_prayers_spec.rb
+++ spec/features/viewing_prayers_spec.rb
@@ -5,38 +5,62 @@ RSpec.feature 'viewing prayers', type: :feature do
     user = FactoryGirl.create(:user)
     sign_in user
 
+    prayer1 = 'Custom prayer'
+    prayer2 = 'Another prayer'
+    updated_prayer2 = 'Enhanced prayer'
+
+    creating_first_prayer_displays_that_prayer(prayer1)
+    next_when_only_one_prayer_redisplays(prayer1)
+    creating_additional_prayer_displays_that_prayer(prayer2)
+    editing_prayer_redisplays_same_prayer(updated_prayer2)
+    next_displays(prayer1)
+    deleting_prayer_displays(updated_prayer2)
+  end
+
+  private
+
+  def creating_first_prayer_displays_that_prayer(prayer)
     visit '/'
 
-    fill_in :prayer_text, with: 'Custom prayer'
+    fill_in :prayer_text, with: prayer
     click_on 'Save Prayer'
 
     expect(page).to have_content('Prayer created')
-    expect(page).to have_content('Custom prayer')
+    expect(page).to have_content(prayer)
+  end
 
+  def next_when_only_one_prayer_redisplays(prayer)
     click_on 'Next'
+    expect(page).to have_content(prayer)
+  end
 
-    expect(page).to have_content('Custom prayer')
-
+  def creating_additional_prayer_displays_that_prayer(prayer)
     click_on 'Add Prayer'
 
-    fill_in :prayer_text, with: 'Another prayer'
+    fill_in :prayer_text, with: prayer
     click_on 'Save Prayer'
 
-    expect(page).to have_content('Another prayer')
+    expect(page).to have_content(prayer)
+  end
 
+  def editing_prayer_redisplays_same_prayer(prayer)
     click_on 'Edit'
-    fill_in :prayer_text, with: 'Enhanced prayer'
+    fill_in :prayer_text, with: prayer
     click_on 'Save Prayer'
 
     expect(page).to have_content('Prayer updated')
-    expect(page).to have_content('Enhanced prayer')
+    expect(page).to have_content(prayer)
+  end
 
+  def next_displays(prayer)
     click_on 'Next'
+    expect(page).to have_content(prayer)
+  end
 
-    expect(page).to have_content('Custom prayer')
-
+  def deleting_prayer_displays(prayer)
     click_on 'Delete'
 
     expect(page).to have_content('Prayer deleted')
+    expect(page).to have_content(prayer)
   end
 end
