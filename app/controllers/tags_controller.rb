1d6b546067595c1d8c84a72017f45302c4e30a36
Josh Justice <jjustice@bignerdranch.com>
2017-09-22 06:34:49 -0400

    Order tags and prayers on tag page

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,11 +2,14 @@
 
 class TagsController < ApplicationController
   def index
-    @tags = ActsAsTaggableOn::Tag.all
+    @tags = ActsAsTaggableOn::Tag.all.order(:name)
   end
 
   def show
     @tag = ActsAsTaggableOn::Tag.find(params[:id])
-    @prayers = Prayer.joins(:tags).where('tags.id' => @tag.id)
+    @prayers = current_user.prayers
+                           .joins(:tags)
+                           .where('tags.id' => @tag.id)
+                           .order(:text)
   end
 end
