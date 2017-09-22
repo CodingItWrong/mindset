6e31529581703528a205ac38a14659afaed102a5
Josh Justice <jjustice@bignerdranch.com>
2017-09-22 07:50:17 -0400

    Fix depracation warning in tag initializer

--- config/initializers/acts_as_taggable_on.rb
+++ config/initializers/acts_as_taggable_on.rb
@@ -1,2 +1,12 @@
-ActsAsTaggableOn.delimiter = ' '
+# frozen_string_literal: true
+
+class SpaceParser < ActsAsTaggableOn::GenericParser
+  def parse
+    ActsAsTaggableOn::TagList.new.tap do |tag_list|
+      tag_list.add @tag_list.split(' ')
+    end
+  end
+end
+
+ActsAsTaggableOn.default_parser = SpaceParser
 ActsAsTaggableOn.remove_unused_tags = true
