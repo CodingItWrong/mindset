21f2a527c0d8d3d9de3c784b773e1aceeb7108eb
Josh Justice <jjustice@bignerdranch.com>
2017-11-02 15:32:33 -0400

    enable user/password auth

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -2,6 +2,13 @@ Doorkeeper.configure do
   # Change the ORM that doorkeeper will use (needs plugins)
   orm :active_record
 
+  resource_owner_from_credentials do |routes|
+    user = User.find_for_database_authentication(email: params[:username])
+    if user&.valid_for_authentication? { user.valid_password?(params[:password]) }
+      user
+    end
+  end
+
   # This block will be called to check whether the resource owner is authenticated or not.
   resource_owner_authenticator do
     fail "Please configure doorkeeper resource_owner_authenticator block located in #{__FILE__}"
@@ -110,3 +117,5 @@ Doorkeeper.configure do
   # WWW-Authenticate Realm (default "Doorkeeper").
   # realm "Doorkeeper"
 end
+
+Doorkeeper.configuration.token_grant_types << 'password'
